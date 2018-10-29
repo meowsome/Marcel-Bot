74dd9f890f14e52e857db0b862c21ac0f9c7fa7d
meowsome <tycarr01@gmail.com>
2018-10-28 23:28:34 -0400

    Removin day

--- app.js
+++ app.js
@@ -1156,62 +1156,11 @@ client.on('message', async message => {
                         })
                         break;
 
-                    case 'date':
-                    case 'today':
+                    case 'dice':
                         if (runCheck % 71 === 0) {
                             break;
                         } else {
                             runCheck *= 71;
-                        }
-
-                        var newDate = new Date();
-
-                        var weekDay = newDate.getDay();
-                        var dayImage = "http://marcel.vulpix.pw/images/weekdays/";
-                        if (weekDay === 0) dayImage += "sunday.jpg";
-                        if (weekDay === 1) dayImage += "monday.jpg";
-                        if (weekDay === 2) dayImage += "tuesday.jpg";
-                        if (weekDay === 3) dayImage += "wednesday.jpg";
-                        if (weekDay === 4) dayImage += "thursday.jpg";
-                        if (weekDay === 5) dayImage += "friday.jpg";
-                        if (weekDay === 6) dayImage += "saturday.jpg";
-
-                        var date = newDate.getDate();
-                        var daysArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-                        var day = daysArray[newDate.getDay()];
-                        var monthsArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-                        var month = monthsArray[newDate.getMonth()];
-                        var year = newDate.getFullYear();
-                        var dateExtra;
-                        if (date === 1 || date === 21 || date === 31) {
-                            dateExtra = 'st'
-                        }
-                        if (date === 2 || date === 22) {
-                            dateExtra = 'nd';
-                        }
-                        if (date === 3 || date === 23) {
-                            dateExtra = 'rd';
-                        }
-                        if (date >= 4 && date <= 20 || date >= 24 && date <= 30) {
-                            dateExtra = 'th';
-                        }
-
-                        message.channel.send({
-                            embed: {
-                                color: 3066993,
-                                description: `Today is **${day}, ${month} ${date}${dateExtra}, ${year}**.`,
-                                "image": {
-                                    "url": dayImage
-                                }
-                            }
-                        });
-                        break;
-
-                    case 'dice':
-                        if (runCheck % 73 === 0) {
-                            break;
-                        } else {
-                            runCheck *= 73;
                         }
                         var diceImagesRandom = Math.round(Math.random() * 5 + 1);
                         message.channel.send({
